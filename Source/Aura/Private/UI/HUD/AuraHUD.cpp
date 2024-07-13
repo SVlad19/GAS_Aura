fa4bb394d40297415349daeacaa9ff9d705a511f
SVlad19 <mr.vladik.semeno3@gmail.com>
2024-07-13 12:44:35 +0300

    Added HUD

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/HUD/AuraHUD.h"
+#include "UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(),OverlayWidgetClass);
+	Widget->AddToViewport();
+
+}
